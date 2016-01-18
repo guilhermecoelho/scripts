65303ce8a28a9e7b36f874c4baca357a705ae4b3
guilherme coelho <guilhermecoelho2@gmail.com>
2016-01-18 10:27:29 -0200

    scritp iptables

---
+++ iptables.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#limpando tabelas
+iptables -F &&
+iptables -X &&
+iptables -t nat -F &&
+iptables -t nat -X &&
+
+#Libera o loopback
+iptables -A INPUT -i lo -j ACCEPT
+
+#Libera SSh e  Web
+iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 3001 -j ACCEPT
+
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+echo 1 > /proc/sys/net/ipv4/ip_forward &&
+# Protecao contra port scanners ocultos
+iptables -A INPUT -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j ACCEPT
+# Bloqueando tracertroute
+iptables -A INPUT -p udp -s 0/0 -i eth1 --dport 33435:33525 -j DROP
+#Protecoes contra ataques
+iptables -A INPUT -m state --state INVALID -j DROP
+#termina
+echo "Iptables Pronto"
