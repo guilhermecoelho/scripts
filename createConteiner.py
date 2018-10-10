af435db625011fce1b512863d0df2f4968a6d38d
Guilherme Coelho <guilhermecoelho2@gmail.com>
2018-10-10 22:12:32 +0100

    Update createConteiner.py

--- createConteiner.py
+++ createConteiner.py
@@ -7,9 +7,8 @@ containerName = raw_input()
 os.system("apt-get install lxd")
 
 os.system("lxc launch ubuntu:x "+containerName)
-os.system("lxc-start -n "+containerName+" -d")
 
-ipContainer = str(os.system("lxc list "+containerName+" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}'"))
+ipContainer = str(os.system("lxc list "+containerName+" -c 4 | awk '!/IPV4/{ if ( $2 != \"\" ) print $2}'"))
           
 os.system("iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to "+ipContainer+":80")
 
