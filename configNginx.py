afda632d1817148881dfc0547a08e5387d23f86a
Guilherme Coelho <guilhermecoelho2@gmail.com>
2018-10-12 00:32:00 +0100

    Update configNginx.py

--- configNginx.py
+++ configNginx.py
@@ -1,23 +1,53 @@
-import sys
+
 import os
+import subprocess
+import time
 
-def confNginx(ip):
-    strConfig = str("server { \n" 
-    "  listen 80;\n"
-    "  server_name tddnode.com;\n"
-    "  location / {\n"
-    "    proxy_pass       http://"+ip+";\n"
-    "    proxy_set_header Host              $host;\n"
-    "    proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;\n"
-    "    proxy_set_header X-Forwarded-Proto $scheme;\n"
-    "    proxy_set_header Accept-Encoding   '';\n"
-    "  }\n"
-    "}\n")
+global containerName
+global ipContainer
 
-    file = open("/etc/nginx/sites-available/default", "w")
-    file.truncate(0)
-    file.write(strConfig)
-    file.close()
 
-os.system("apt-get install nginx")
-confNginx(sys.argv[1])
+def createContainer():
+    global containerName
+    global ipContainer
+
+    print("Container name: ")
+    containerName = raw_input()
+
+    os.system("git clone  https://github.com/guilhermecoelho/scripts.git /tmp/script/")
+
+    os.system("apt-get install lxd")
+
+    os.system("lxc launch ubuntu:x "+containerName)
+
+    #need wait a time to mapping container IP
+    os.system("lxc list")
+    time.sleep(10)
+    os.system("lxc list")
+
+    p = subprocess.Popen("lxc list "+containerName+" -c 4 | awk '!/IPV4/{ if ( $2 != \"\" ) print $2}'te", stdout=subprocess.PIPE, shell=True)
+    (ipContainer, err) = p.communicate()
+
+    print "lxc exec "+containerName+" -- sudo apt-get update"
+    os.system("lxc exec "+containerName+" -- sudo apt-get update")
+
+    print "lxc exec "+containerName+" -- sudo apt-get install python"
+    os.system("lxc exec "+containerName+" -- sudo apt-get install python")
+
+def configNginx():
+    global containerName
+    global ipContainer
+
+    print "lxc file push /tmp/script/configNginx.py  "+containerName+"/tmp/configNginx.py"
+    os.system("lxc file push /tmp/script/configNginx.py  "+containerName+"/tmp/configNginx.py")
+
+    print "lxc exec "+containerName+" -- sudo python /tmp/configNginx.py "+str(ipContainer)
+    os.system("lxc exec "+containerName+" -- sudo python /tmp/configNginx.py "+str(ipContainer))
+
+
+createContainer()
+configNginx()
+
+os.system("iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to "+str(ipContainer)+":80")
+
+os.system("rm -r /tmp/script/")
